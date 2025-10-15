1ea9c82c454539250fe2415d60aa6b37a81ce450
Bouchra Bairi <bouchrabairi66@gmail.com>
2025-10-15 17:38:28 +0100

    Update homework4.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
-string s;
-cout<<"Enter a string:";
-cin>>s;
+int main() {
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+    int freq[26] = {0}; 
+    for (int i = 0; i < s.size(); i++) {
+        char c = s[i];
+        freq[c - 'a']++; 
+    }
+    int minFreq = 1000, maxFreq = 0;
 
-int f[26]={0};
-int i;
-for(i=0;i<s.length();i++){
-f[s[i]-'a']++;
-}
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0) { 
+            if (freq[i] < minFreq)
+                minFreq = freq[i];
+            if (freq[i] > maxFreq)
+                maxFreq = freq[i];
+        }
+    }
+    if (minFreq == maxFreq)
+        cout << "The string is valid" << endl;
+    else if ((maxFreq - minFreq == 1) || minFreq == 1)
+        cout << "The string is valid" << endl;
+    else
+        cout << "The string is not valid" << endl;
 
-int ref=0;
-for(i=0;i<26;i++){
-if(f[i]>0){
-ref=f[i];
-break;
-}
-}
-
-int diff=0;
-for(i=0;i<26;i++){
-if(f[i]>0){
-if(f[i]!=ref){
-if(abs(f[i]-ref)>1 && f[i]!=1){
-cout<<"Invalid string"<<endl;
-return 0;
-}
-diff++;
-}
-}
+    return 0;
 }
 
-if(diff<=1)
-cout<<"Valid string"<<endl;
-else
-cout<<"Invalid string"<<endl;
-
-return 0;
-}
